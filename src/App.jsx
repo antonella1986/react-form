94a6e98d603d0ee63e262c1dfbe6363b02a2596c
antonella1986 <antonella.leo.mail@gmail.com>
2025-03-26 16:26:52 +0100

    add input for sumbit new article

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import Articles from './data/Articles'
 function App() {
   //definisco la costante dello stato iniziale
   const [title, setTitle] = useState('')
+  const [newArticle, setNewArticle] = useState('')
 
+  function handleFormSubmit(e) {
+    e.preventDefault()
+  }
 
   return (
     <>
@@ -12,12 +16,25 @@ function App() {
 
         <div className="container">
           {Articles.map((article) => (
-            <button key={article.title} className='btn-primary' onClick={() => setTitle(article.title)}>
+/*             <button key={article.title} className='btn-primary' onClick={() => setTitle(article.title)}>
               Show article title
-            </button>
+            </button> */
+            <h2>{article.title}</h2>
           ))}
-          <div className="card">{title}</div>
         </div>
+
+        <form onSubmit={handleFormSubmit}>
+          <input
+          type="text"
+          className='form-control rounded-0'
+          name='new_article'
+          id='new_article'
+          aria-describedby='title_helper'
+          placeholder='Add article'
+          value={newArticle}
+          onChange={e => setNewArticle(e.target.value)} />
+          <button type='submit' className='btn btn-primary justify-content-center'>Sumbit your article</button>
+        </form>
 
 
     </>
